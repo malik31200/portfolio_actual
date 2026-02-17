2c90671c04f652c2a3458a0a891417fbf8982127
malik31200 <yka-vtc@outlook.fr>
2026-02-17 15:25:21 +0100

    create tee entity

---
+++ migrations/Version20260217142351.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260217142351 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE registration ADD status VARCHAR(20) NOT NULL, ADD registered_at DATETIME NOT NULL, ADD cancelled_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE registration DROP status, DROP registered_at, DROP cancelled_at');
+    }
+}
